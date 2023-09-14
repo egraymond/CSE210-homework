4a67e2be593a0e57bf682318dfae25bd9e0f924f
Elias Garlitz <egraymond225@gmail.com>
2023-09-14 14:07:26 -0600

    Completed Prep 1 and Prep 2 Assignments

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage?: ");
+        string percent = Console.ReadLine();
+        int percentage = int.Parse(percent);
+
+        string grade;
+
+        if (percentage < 0 || percentage > 100)
+        {
+            Console.WriteLine("The current number is out of bounds. Try again");
+            Environment.Exit(0);
+            
+        }
+
+        if (percentage <= 100)
+        {
+            if (percentage >= 90)
+            {
+                grade = "A";
+            }
+            else if (percentage >= 80)
+            {  
+                grade = "B";
+            }
+            else if (percentage >= 70)
+            {  
+                grade = "C";
+            }
+            else if (percentage >= 60)
+            {  
+                grade = "D";
+            }
+            else
+            {
+                grade = "F";
+            }
+
+        if (percentage > 70 && percentage <= 100)
+        {
+            Console.WriteLine($"Your current grade is {grade}");
+            Console.WriteLine("You Pass the Class!");
+        }
+        else
+        {
+            Console.WriteLine("You Failed the Class");
+        }
+        }
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        
     }
 }
