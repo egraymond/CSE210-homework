6b0e6e3127cdab6b06215d35749df66b291e1a19
Elias Garlitz <egraymond225@gmail.com>
2023-09-19 13:30:26 -0600

    Prep 3 had been completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random magicnumber = new Random();
+        int number = magicnumber.Next(0, 21);
+            
+        Console.Write("Please guess a number: ");
+        string guess = Console.ReadLine();
+        int magguess = int.Parse(guess);
+            
+            while (magguess != number)
+            {
+                
+                if (magguess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magguess > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+
+                Console.Write("Try again!: ");
+                guess = Console.ReadLine();
+                magguess = int.Parse(guess);
+            }
+            Console.WriteLine("You guessed it!");
     }
 }
