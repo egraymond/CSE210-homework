1d9856bf11e9228e19e196babb77ef1ee36e27e9
Elias Garlitz <egraymond225@gmail.com>
2023-09-20 14:23:52 -0600

    Completed Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,37 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int favNumber = Convert.ToInt32(Console.ReadLine());
+        return favNumber;
+    }
+    static int SquareNumber(int squaredNum)
+    {
+        return squaredNum * squaredNum;
+    }
+    static void DisplayResult(string userName, int squaredInt)
+    {
+        Console.Write($"{userName}, the square of your number is {squaredInt}");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int favoriteNum = PromptUserNumber();
+        int squared = SquareNumber(favoriteNum);
+
+        DisplayResult(userName, squared);
     }
 }
