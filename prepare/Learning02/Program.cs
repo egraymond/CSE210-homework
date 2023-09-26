9a1d19b2d336c8e5cdc54888b53939f9ee9966c5
Elias Garlitz <egraymond225@gmail.com>
2023-09-26 13:03:57 -0600

    Learning 02(resume) Completed

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Apple";
+        job1._jobTitle = "Developer";
+        job1._startYear = 2009;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._company = "Microsoft";
+        job2._jobTitle = "Team Lead";
+        job2._startYear = 2002;
+        job2._endYear = 2008;
+
+        Resume myResume = new Resume();
+        myResume._name = "Elias Garlitz";
+        
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.Display();
+
+
     }
 }
