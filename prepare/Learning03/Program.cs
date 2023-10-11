5878fd3c315e2febc8e48f559b74e583247a45a5
Elias Garlitz <egraymond225@gmail.com>
2023-10-11 13:31:47 -0600

    completed learning 03 assignment

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fractions f_1 = new Fractions();
+        Fractions f_2 = new Fractions(6);
+        Fractions f_3 = new Fractions(6/2);
+        Fractions f_4 = new Fractions(2/5);
+
+        f_1.GetFractionString();
+        f_1.GetDecimalValue();
+
+        f_2.GetFractionString();
+        f_2.GetDecimalValue();
+
+        f_3.GetFractionString();
+        f_3.GetDecimalValue();
+
+        f_4.GetFractionString();
+        f_4.GetDecimalValue();
     }
 }
+
