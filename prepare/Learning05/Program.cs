21a1f4cc85770839d3c926ef726aaf30255465ad
Elias Garlitz <egraymond225@gmail.com>
2023-11-14 09:48:29 -0700

    Completed Learning 05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -2,8 +2,31 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        // Test Square
+        Square square = new Square("Red", 5.0);
+        Console.WriteLine($"Square - Color: {square.Color}, Area: {square.GetArea()}");
+
+        // Test Rectangle
+        Rectangle rectangle = new Rectangle("Blue", 4.0, 6.0);
+        Console.WriteLine($"Rectangle - Color: {rectangle.Color}, Area: {rectangle.GetArea()}");
+
+        // Test Circle
+        Circle circle = new Circle("Green", 3.0);
+        Console.WriteLine($"Circle - Color: {circle.Color}, Area: {circle.GetArea()}");
+
+        // Add shapes to the list
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        // Iterate through the list and display color and area for each shape
+        foreach (var shape in shapes)
+        {
+            Console.WriteLine($"Shape - Color: {shape.Color}, Area: {shape.GetArea()}");
+        }
     }
 }
---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,21 @@
+public class Shape
+{
+    private string _color;
+
+    public string Color
+    {
+        get { return _color; }
+        set { _color = value; }
+    }
+
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    public virtual double GetArea()
+    {
+        // To be overridden by derived classes
+        return 0.0;
+    }
+}
